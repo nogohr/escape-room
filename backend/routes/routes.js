cf1f205ca89edbf0d61ab001a8bfc061a58b18f7
Lars Van Houwelingen <lars@keytoe.nl>
2020-04-20 13:23:38 +0200

    Fix await promise & correct host for mysql

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const models = require('../models');
+const models = require("../models");
 const EscapeRoom = models.EscapeRoom;
 
-router.get('/', (req, res, next) => {
-    const result = {
-        'data': 'The server is running',
-        'statusCode': 200
-    };
+router.get("/", (req, res, next) => {
+  const result = {
+    data: "The server is running",
+    statusCode: 200,
+  };
 
-    res.status(200).json(result);
+  res.status(200).json(result);
 });
 
-router.get('/escape-rooms', async (req, res, next) => {
-    const escapeRooms = await EscapeRoom.findAll();
-
-    const result = {
-        'data': escapeRooms,
-        'statusCode': 200
-    };
-
-    res.status(200).json(result);
+router.get("/escape-rooms", async (req, res, next) => {
+  let result = {};
+  await EscapeRoom.findAll()
+    .then((response) => {
+      result = {
+        data: response,
+        statusCode: 200,
+      };
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log("Error fetching Rooms", err);
+      res.status(500).json(result);
+    });
 });
 
 exports.routeHandler = router;
