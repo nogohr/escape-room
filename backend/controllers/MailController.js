6973b5ca11963e33dfb0c7233810b585ffd879c6
Jory Kalisvaart <jkalisvaart@hotmail.com>
2020-05-18 15:38:01 +0200

    Mail - WIP

--- backend/controllers/MailController.js
+++ backend/controllers/MailController.js
@@ -1,23 +1,48 @@
 const sgMail = require('@sendgrid/mail');
+const path = require('path');
+const ejs = require('ejs');
+
+const config = require('../config/config.json');
+const SEND_GRID_API_KEY = config.sendGridApiKey;
 
 exports.sendMail = async function (req, res) {
-    sgMail.setApiKey("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");
+
+    return res.render('mail.ejs', {
+        customer_name: 'Henk de Tester'
+    });
+
+
+    const html = await ejs
+    .renderFile(path.join(__dirname, '../views/mail.ejs'),
+    {
+        customer_name: 'capitalizedFirstName'
+    });
+
+    res.render(html);
+
+
+    console.log(1234, html)
+    return;
+    
+    sgMail.setApiKey(SEND_GRID_API_KEY);
 
     const msg = {
-        to: '0983144@hr.nl',
+        to: 'nogohr@protonmail.com',
         from: 'nogohr@protonmail.com',
-        subject: 'Sending with Twilio SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
+        subject: 'Bestiging van reservering voor escape room ESCAPE_ROOM_NAAM',
+        text: 'Bestiging van reservering voor escape room',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
 
     try {
         const res = await sgMail.send(msg);
         console.log('res', res);
+        console.log('Mail has been successfully send');
         
-    } catch (err) {
-        console.log('err', err.response.body);
-        
+    } catch (error) {
+        res.status(500).json({
+            error
+        });
     }
 
     res.status(200).json({});
